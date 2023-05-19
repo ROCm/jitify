e4255d14644925f1a84451556ee814c606953b77
Robert Maynard <rmaynard@nvidia.com>
2023-05-19 09:19:14 -0400

    Ensure that we still provide `std::` fixed width types

--- jitify2.hpp
+++ jitify2.hpp
@@ -3580,8 +3580,43 @@ JITIFY_DEFINE_C_AND_CXX_HEADERS(stdint, R"(
 #include <climits>
 #include <cuda/std/climits>
 #include <cuda/std/cstdint>
+#define _JITIFY_WCHAR_T_IS_UNSIGNED ((wchar_t)-1 >= 0)
+#define WCHAR_MIN                                                      \
+    (sizeof(wchar_t) == 2 ? _JITIFY_WCHAR_T_IS_UNSIGNED ? 0 : SHRT_MIN \
+                          : _JITIFY_WCHAR_T_IS_UNSIGNED ? 0 : INT_MIN)
+#define WCHAR_MAX                                                              \
+    (sizeof(wchar_t) == 2 ? _JITIFY_WCHAR_T_IS_UNSIGNED ? USHRT_MAX : SHRT_MAX \
+                          : _JITIFY_WCHAR_T_IS_UNSIGNED ? UINT_MAX : INT_MAX)
 )",
-R"(
+                                R"(
+typedef signed char int8_t;
+typedef signed short int16_t;
+typedef signed int int32_t;
+typedef signed long long int64_t;
+typedef signed char int_fast8_t;
+typedef signed short int_fast16_t;
+typedef signed int int_fast32_t;
+typedef signed long long int_fast64_t;
+typedef signed char int_least8_t;
+typedef signed short int_least16_t;
+typedef signed int int_least32_t;
+typedef signed long long int_least64_t;
+typedef signed long long intmax_t;
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long uint64_t;
+typedef unsigned char uint_fast8_t;
+typedef unsigned short uint_fast16_t;
+typedef unsigned int uint_fast32_t;
+typedef unsigned long long uint_fast64_t;
+typedef unsigned char uint_least8_t;
+typedef unsigned short uint_least16_t;
+typedef unsigned int uint_least32_t;
+typedef unsigned long long uint_least64_t;
+typedef unsigned long long uintmax_t;
+
 )");
 
 JITIFY_DEFINE_C_AND_CXX_HEADERS(stdio, "#include <cstddef>", R"(
