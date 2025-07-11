215193349556807843461217d654528438688e18
monoatamd <mnorouzi@amd.com>
2025-07-11 11:24:43 +0000

    jitify2_test_kernels.cu: 1. nvrtc seems to automatically include cwd into include paths; for hiprtc, we manually add the cwd to the include paths to fix compilation errors 2. Fixed missing declaration of pointless_func (nvcc behaves differently and does not complain about missing declaration)
    example_headers/class_arg_kernel.cuh:
    1. Used HIPRTC gaurds instead of CUDA
    
    example_headers/my_header2.cuh:
    1. Used device attrib
    
    example_headers/my_header3.cuh:
    1. Used device attrib

--- example_headers/class_arg_kernel.cuh
+++ example_headers/class_arg_kernel.cuh
@@ -26,23 +26,45 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+// MIT License
+//
+// Modifications Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #pragma once
 
 class Managed {
  public:
   void* operator new(size_t len) {
     void* ptr = nullptr;
-#ifndef __CUDACC_RTC__
-    cudaMallocManaged(&ptr, len);
-    cudaDeviceSynchronize();
+#ifndef __HIPCC_RTC__
+    (void)cudaMallocManaged(&ptr, len);
+    (void)cudaDeviceSynchronize();
 #endif
     return ptr;
   }
 
   void operator delete(void* ptr) {
-#ifndef __CUDACC_RTC__
-    cudaDeviceSynchronize();
-    cudaFree(ptr);
+#ifndef __HIPCC_RTC__
+    (void)cudaDeviceSynchronize();
+    (void)cudaFree(ptr);
 #endif
   }
 };
--- jitify2_test_kernels.cu
+++ jitify2_test_kernels.cu
@@ -26,11 +26,36 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+// MIT License
+//
+// Modifications Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 // This file is preprocessed offline and then used in the test suite.
 
 #include "example_headers/my_header1.cuh"
 #include "example_headers/my_header2.cuh"
 #include "example_headers/my_header3.cuh"
+
+template <typename T>
+__device__ T pointless_func(T x);
 
 __global__ void my_kernel1(const float* indata, float* outdata) {
   outdata[0] = indata[0] + 1;
