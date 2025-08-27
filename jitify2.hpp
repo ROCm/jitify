f33b491bfdc41b3a3a7c9236de99e27492996525
NorouziArab, Mohammad <Mohammad.NorouziArab@amd.com>
2025-08-27 11:25:50 +0200

    jitify2.hpp: Use c++11

--- jitify2.hpp
+++ jitify2.hpp
@@ -2792,7 +2792,7 @@ inline bool process_architecture_flags(StringVec* compiler_options,
 }
 // NOTE(HIPRTC): we should use c++11. Issue #54.
 inline void add_std_flag_if_not_specified(StringVec* options,
-                                          std::string value = "c++17") {
+                                          std::string value = "c++11") {
   for (const std::string& option : *options) {
     if (option.find("--std") != std::string::npos ||
         option.find("-std") != std::string::npos) {
@@ -3241,7 +3241,7 @@ inline CompiledProgram CompiledProgram::compile(
                                           &error)) {
     return Error("Failed to process architecture flags: " + error);
   }
-  detail::add_std_flag_if_not_specified(&compiler_options, "c++17"); // NOTE(HIPRTC): we should use c++11. Issue #54.
+  detail::add_std_flag_if_not_specified(&compiler_options, "c++11"); // NOTE(HIPRTC): we should use c++11. Issue #54.
   detail::add_cwd_include_path(&compiler_options);
   // NOTE(HIPRTC): Not supported.
   // bool should_remove_unused_globals = detail::pop_flag(
@@ -5374,7 +5374,7 @@ inline PreprocessedProgram PreprocessedProgram::preprocess(
         detail::get_jitsafe_headers_map().at("jitify_preinclude.h"));
     compiler_options.push_back("-includejitify_preinclude.h");
   }
-  detail::add_std_flag_if_not_specified(&compiler_options, "c++17");
+  detail::add_std_flag_if_not_specified(&compiler_options, "c++11");
   detail::add_cwd_include_path(&compiler_options);
   bool minify = detail::pop_flag(&compiler_options, "-m", "--minify");
   // TODO: This flag is experimental, because the implementation does not
